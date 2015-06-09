aef9786be0d07989c636b07d685a6b3e3f4de594
Yury Shapkarin <yury@shapkarin.me>
2015-06-09 03:09:52 +0300

    tabs to spaces

--- app/app.js
+++ app/app.js
@@ -1,90 +1,90 @@
 define(['jquery',
         'underscore',
-       	'backbone',
+         'backbone',
 
-	'app/collections/search_list',
-	'app/models/track',
+  'app/collections/search_list',
+  'app/models/track',
 
-	'app/views/search_bar',
-	'app/views/history',
-	'app/views/search_results',
-	
-	'text!app/templates/layout.tp',
-	'text!app/templates/search_results/artist.tp',
-	'text!app/templates/search_results/tag.tp'
-	],
+  'app/views/search_bar',
+  'app/views/history',
+  'app/views/search_results',
+  
+  'text!app/templates/layout.tp',
+  'text!app/templates/search_results/artist.tp',
+  'text!app/templates/search_results/tag.tp'
+  ],
 
-	function( $, _, Backbone,
+  function( $, _, Backbone,
 
-		  SearchList,
-		  Artist,
+      SearchList,
+      Artist,
 
-		  SearchBar,
-		  History,
-		  SearchResults,
+      SearchBar,
+      History,
+      SearchResults,
 
-		  layoutTp,
-		  artistTp,
-		  tagTp
-	) {
+      layoutTp,
+      artistTp,
+      tagTp
+  ) {
 
-			var App = Backbone.View.extend({
+      var App = Backbone.View.extend({
 
-				events: {
-					'search .search_bar': 'newSearch',
-					'search .history': 'historySearch'
-				},
+        events: {
+          'search .search_bar': 'newSearch',
+          'search .history': 'historySearch'
+        },
 
-				initialize: function() {
-				
-					this.$el.html(_.template(layoutTp, {}));
+        initialize: function() {
+        
+          this.$el.html(_.template(layoutTp, {}));
 
-					this.searchBar = new SearchBar({
-						el: this.$el.find('.search_bar')
-					});
+          this.searchBar = new SearchBar({
+            el: this.$el.find('.search_bar')
+          });
 
           this.history = new History({
-						el: this.$el.find('.history'),
-						searches: new SearchList()
-					});
+            el: this.$el.find('.history'),
+            searches: new SearchList()
+          });
 
-					this.artistSearchResults = new SearchResults({
-						el: $('#search_results'),
-					        model: new Artist,
-					        template: artistTp	
-					});
+          this.artistSearchResults = new SearchResults({
+            el: $('#search_results'),
+                  model: new Artist,
+                  template: artistTp  
+          });
 
-					this.tagSearchResults = new SearchResults({
-						el: $('#search_results'),
-					        model: new Artist,
-					        template: tagTp
-					});
-				},
+          this.tagSearchResults = new SearchResults({
+            el: $('#search_results'),
+                  model: new Artist,
+                  template: tagTp
+          });
+        },
 
-				newSearch: function(ev, search) {
-				
-					this.history.addSearch(search);	
+        newSearch: function(ev, search) {
+        
+          this.history.addSearch(search);  
 
-					this.performSearch(search);
-				},
+          this.performSearch(search);
+        },
 
-				historySearch: function(ev, search) {
-				
-					this.performSearch(search);
-				},
+        historySearch: function(ev, search) {
+        
+          this.performSearch(search);
+        },
 
-				performSearch: function(search) {
+        performSearch: function(search) {
 
           var service = search && search.service,
-					    viewId = service + 'SearchResults',
-					    view = this[viewId];
-					  
-					if(view) {
-						view.search(search);
-					}
-				}
+              viewId = service + 'SearchResults',
+              view = this[viewId];
+            
+          if(view) {
+            view.search(search);
+          }
+        }
 
       });
 
-			return App;
-	});
+      return App;
+  });
