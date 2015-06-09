ca1ddd335537398b51fccd62f3d52bee733aca52
Yury Shapkarin <yury@shapkarin.me>
2015-06-09 03:10:18 +0300

    tabs to spaces

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 requirejs.config({
-	baseUrl: '/',
-	paths: {
-		text: 'lib/require/plugins/text',
-		jquery: 'lib/jquery-1.8.3',
-		underscore: 'lib/underscore',
-		backbone: 'lib/backbone'
-	}
+  baseUrl: '/',
+  paths: {
+    text: 'lib/require/plugins/text',
+    jquery: 'lib/jquery-1.8.3',
+    underscore: 'lib/underscore',
+    backbone: 'lib/backbone'
+  }
 
 });
 
 requirejs(['jquery', 
-	   'underscore',
-	   'backbone',
-	   'app/app'], function($, _, Backbone, App) {
+     'underscore',
+     'backbone',
+     'app/app'], function($, _, Backbone, App) {
 
-		new App({
-			el: $('#application')
-		});
+    new App({
+      el: $('#application')
+    });
 });
