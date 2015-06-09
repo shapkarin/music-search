d68f1ff3ed2aaa75362445ed5fe1e22bdcb78868
Yury Shapkarin <yury@shapkarin.me>
2015-06-10 00:02:38 +0300

    init

--- app/templates/layout.js
+++
@@ -1,7 +0,0 @@
-function template(locals) {
-var buf = [];
-var jade_mixins = {};
-var jade_interp;
-
-buf.push("<header class=\"navbar navbar-default\"><div class=\"container-fluid\"><div class=\"navbar-header\"><a href=\"/\" class=\"navbar-brand\">Top Tracks</a></div><div class=\"search_bar\"></div><ul class=\"nav navbar-nav navbar-right\"><li><a href=\"https://github.com/shapkarin/music-search\" target=\"_blank\"><i class=\"icon-git\"></i></a></li></ul></div></header><div class=\"history\"></div><div id=\"search_results\"></div>");;return buf.join("");
-}
--- app/templates/search_bar.js
+++
@@ -1,7 +0,0 @@
-function template(locals) {
-var buf = [];
-var jade_mixins = {};
-var jade_interp;
-
-buf.push("<form role=\"search\" class=\"navbar-form navbar-left search_form\"><div class=\"form-group\"><div class=\"input-group\"><input id=\"query\" type=\"text\" name=\"query\" value=\"\" placeholder=\"Артист или стиль\" class=\"form-control\"/><div class=\"input-group-btn\"><button style=\"display:block;\" class=\"search btn btn-default\">Поиск<i class=\"icon-search\"></i></button></div></div><div class=\"radio\"><label for=\"artist\" class=\"radio-inline\"><input id=\"artist\" type=\"radio\" name=\"service\" value=\"artist\" checked=\"\"/><i class=\"icon-user\"></i> Артист</label></div><div class=\"radio\"><label for=\"tag\"><input id=\"tag\" type=\"radio\" name=\"service\" value=\"tag\"/><i class=\"icon-hash\"></i> Тег</label></div></div></form>");;return buf.join("");
-}
