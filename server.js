184c60df7297f48f4abaea95f4b37943a5224219
Yury Shapkarin <yury@shapkarin.me>
2015-06-11 15:20:14 +0300

    server

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require('express'),
   modRewrite = require('connect-modrewrite'),
   app = express(),
-  port = 3000;
+  port = 3000,
+  exclude = 'js,css,svg,jade,woff,ttf,svg,eot';
 
 app.set('port', port);
 
 app.set('views', __dirname);
 
-app.use(modRewrite([ '!\.(js|css|svg) /index.html [L]' ]));
+app.use(modRewrite([ '!\.(' + exclude.replace(/,/g, '|') + ') /index.html [L]' ]));
 
-app.use(express.static(__dirname + '/output'));
+app.use(express.static(__dirname));
 
 app.listen(port, function(){
   console.log('Откройте страницу: http://localhost:%s', port);
